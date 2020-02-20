97d6c0cce4cd22eb4e6b69cc21d56d0c4caba083
Khang Vo <kinvo5120@gmail.com>
2020-02-20 10:10:48 +0700

    Khang inital folder

---
+++ J1.L.P0022/src/j1/l/p0022/J1LP0022.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package j1.l.p0022;
+
+/**
+ *
+ * @author khangvo
+ */
+public class J1LP0022 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
