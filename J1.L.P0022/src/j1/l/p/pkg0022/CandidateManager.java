a4604514304425c84a08da70b102ad082c0c41f7
SubereV <kinvo5120@gmail.com>
2020-03-15 09:56:17 +0700

    Khang Build basic class

---
+++ J1.L.P0022/src/j1/l/p/pkg0022/CandidateManager.java
@@ -0,0 +1,19 @@
+/*
+ * Name: Vo Nhu Khang
+ * Class: SE1403
+ * MSSV: DE140179
+ */
+package j1.l.p.pkg0022;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ *
+ * @author khangvn
+ */
+public class CandidateManager {
+
+    private static final List<Candidate> clist = new ArrayList<>();
+
+}
