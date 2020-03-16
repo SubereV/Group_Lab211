2c8fdbc1e07cb5839a9ba09183e02f14f9954405
Đinh Nghĩa <dnpn@MacBook-Pro-cua-inh.local>
2020-03-16 20:52:30 +0700

    nghia jsp76

--- J1.S.P0076/src/j1/s/p0076/J1SP0076.java
+++ J1.S.P0076/src/j1/s/p0076/J1SP0076.java
@@ -5,6 +5,8 @@
  */
 package j1.s.p0076;
 
+import java.util.ArrayList;
+
 /**
  *
  * @author khangvo
@@ -16,6 +18,38 @@ public class J1SP0076 {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        ArrayList<CSV> ls = new ArrayList<>();
+        ls.add(new CSV(1, "dinh phu nghia", "nghia@gmail.com", "0968038714", "bo trach - Quang BINH"));
+        ls.add(new CSV(2, "nguyen    van a ", "aaaa@gmail.com", "0984481345", "Thanh khe - Da NaNG"));
+        ls.add(new CSV(3, "Nguyen tran c     	", "ccccccc@gmail.com", "0986246814", "Lien chieU     - Da    Nang    "));
+        while (true) {
+            System.out.println("1. Import CSV");
+            System.out.println("2. Format Address");
+            System.out.println("3. Format Name");
+            System.out.println("4. Export CSV");
+            System.out.println("5. Exit");
+            System.out.print("Please choice one option:");
+            int n = Manager.checkinputLimit(1, 5);
+            switch (n) {
+                case 1:
+                    Manager.importFile(ls);
+                    Manager.print(ls);
+                    break;
+                case 2:
+                    Manager.formatAddress(ls);
+                    Manager.print(ls);
+                    break;
+                case 3:
+                    Manager.formatName(ls);
+                    Manager.print(ls);
+                    break;
+                case 4:
+                    Manager.exportFile(ls);
+                    break;
+                case 5:
+                    return;
+            }
+        }
     }
     
 }
--- J1.S.P0077/src/j1/s/p0077/J1SP0077.java
+++ J1.S.P0077/src/j1/s/p0077/J1SP0077.java
@@ -9,13 +9,40 @@ package j1.s.p0077;
  *
  * @author khangvo
  */
-public class J1SP0077 {
-
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        // TODO code application logic here
-    }
+public class J1SP0077 
+{
     
+    public static void main(String[] args) {
+
+    
+      
+        String path;
+        String word;
+        while (true) {
+            System.out.println("1. Count Word In File");
+            System.out.println("2. Find File By Word");
+            System.out.println("3. Exit");
+            int choice = Manager.checkinputLimit(1, 3);
+            switch (choice) {
+                case 1:
+                    System.out.print("Enter Path: ");
+                    path = Manager.checkinputString();
+                    System.out.print("Enter Word: ");
+                    word = Manager.checkinputString();
+                    int count = Manager.countWordInFile(path, word);
+                    System.out.println("Count: " + count);
+                    break;
+                case 2:
+                    System.out.print("Enter Path: ");
+                    path = Manager.checkinputString();
+                    System.out.print("Enter Word: ");
+                    word = Manager.checkinputString();
+                    Manager.getFileNameContainsWordInDirectory(path, word);
+                    break;
+                case 3:
+                    return;
+            }
+        }
+    }
 }
+
