f7177b0b39344683c5303c5fd9033ffd3f84b112
Đinh Nghĩa <dnpn@MacBook-Pro-cua-inh.local>
2020-03-20 16:02:37 +0700

    nghia addExp

--- J1.L.P0022/src/j1/l/p/pkg0022/CandidateManager.java
+++ J1.L.P0022/src/j1/l/p/pkg0022/CandidateManager.java
@@ -17,6 +17,37 @@ public class CandidateManager {
 
     private static final List<Candidate> clist = new ArrayList<>();
     private static final Scanner input = new Scanner(System.in);
-    
-    
+
+    public static void addExperience() {
+
+        int type = 0;
+
+        System.out.print("Enter id: ");
+        String id = Validation.inputString();
+        System.out.print("Enter first name: ");
+        String firstName = Validation.inputString();
+        System.out.print("Enter last name: ");
+        String lastName = Validation.inputString();
+        System.out.print("Enter birth date: ");
+        int birthDate = Validation.BirthDate();
+        System.out.print("Enter address: ");
+        String address = Validation.inputString();
+        System.out.print("Enter phone: ");
+        String phone = Validation.Phone();
+        System.out.print("Enter email: ");
+        String email = Validation.Email();
+        Candidate candidate = new Candidate(id, firstName, lastName,
+                birthDate, address, phone, email, type);
+
+        System.out.print("Enter year of experience: ");
+        int yearExperience = Validation.checkInputExprience(candidate.getBirthDate());
+        System.out.print("Enter professional skill: ");
+        String professionalSkill = Validation.inputString();
+        clist.add(new Experience(yearExperience, professionalSkill,
+                candidate.getCandidateId(), candidate.getFirstName(), candidate.getLastName(),
+                candidate.getBirthDate(), candidate.getAddress(),
+                candidate.getPhone(), candidate.getEmail(), candidate.getCandidateType()));
+        System.err.println("Create success.");
+    }
+
 }
